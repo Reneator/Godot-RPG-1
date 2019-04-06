5bb6ca2277cb9cc5fcd36f198824de146fb93dc8
Reneator <Reneator@googlemail..com>
2019-04-06 20:58:18 +0200

    Added selfmade Assets
    Just for Tests and Stuff

---
+++ Assets/selfmade/AttackBox.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Objects.Level;
+using UnityEngine;
+
+public class AttackBox : MonoBehaviour
+{
+    public double attackDuration = 0.5;
+    private List<Enemy> _enemiesHit = new List<Enemy>();
+    private DateTime awakeTime;
+    private Player player;
+
+    private void Awake()
+    {
+        Debug.Log("AttackBox woke up!");
+        awakeTime = DateTime.Now;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<Player>();
+        Debug.Log("AttackBox started!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((DateTime.Now - awakeTime).Seconds > attackDuration)
+        {
+            Debug.Log("AttackBox put itself back to sleep!");
+            _enemiesHit = new List<Enemy>();
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        awakeTime = DateTime.Now;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Damageable"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if (!_enemiesHit.Contains(enemy))
+            {
+                _enemiesHit.Add(enemy);
+                Debug.Log(other.ToString());
+
+                player.Damage(enemy);
+            }
+        }
+    }
+}
---
+++ Assets/selfmade/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    private bool movement = false;
+    private int slowDownFactor = 2;
+    public int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!movement)
+        {
+            SlowDown();
+
+           
+        }
+    }
+    
+    private void SlowDown()
+    {
+        rb2d.velocity = rb2d.velocity * 1/slowDownFactor;
+    }
+
+    public void GetDamaged(int damage)
+    {
+        health -= damage;
+        Debug.Log("Enemy Health: "+health);
+    }
+}
